65951f88f06cacde91623372173191f2e37b46bf
Michael Pham <michael.pham67@gmail.com>
2019-06-27 18:11:36 -0400

    my first cis 4004 project 7 commit

---
+++ myscript.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+			function getCurrentDate(){
+				var curDate=new Date();
+				document.getElementById("showDate").innerHTML = curDate.toString();
+			}
+        document.write("Today is: ");
+		</script>
+    <!--Here date and time will display-->
+    <span id="showDate"></span>
+
+//http://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
+//
